73cec498af2cddd573d7b99ae80e0cf39f823c01
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2022-11-26 14:58:51 +0300

    alertSection Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './components/header'
+import AlertSection from './components/alertSection'
+import './components/css/styles.css'
 
  function App() {
   return (
-    <div> 
+    <body> 
        <Header /> 
-       </div>
+       <AlertSection />
+
+      </body>
    
   )
 }
